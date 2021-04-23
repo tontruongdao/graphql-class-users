55beb2b30e7871d656f1c37841f71b362c10c3e7
Truong Dao <ton.truong.dao@outlook.com>
2021-04-23 15:06:01 -0400

    Resolved circular references with callback

--- schema/schema.js
+++ schema/schema.js
@@ -4,22 +4,30 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
 
-
+// Callback on the field is defined but only executed when the entire file has been executed
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType), // As there may be multiple users
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                .then(res => res.data)
+      }
+    }
+  }),
 })
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -30,7 +38,7 @@ const UserType = new GraphQLObjectType({
                 .then(res => res.data)
       }
     }
-  }
+  })
 })
 
 const RootQuery = new GraphQLObjectType({
